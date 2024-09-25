4a48c58aa8650d9027653201983c0c9a37e923ee
EKil <loreleyko@gmail.com>
2024-09-26 00:21:27 +0200

    Added requirements

--- src/ait/textManipulation/actions/Action.java
+++ src/ait/textManipulation/actions/Action.java
@@ -3,6 +3,13 @@ package ait.textManipulation.actions;
 import java.util.Collections;
 import java.util.List;
 
+/* Additional requirements to the user that is adding his/her actions:
+1. The new action must be added as a public method to the Action class.
+2. The action name must be unique and must have the following signature: public void actionName(List<String> rows)
+3. The action method should always accept a List<String> parameter that contains the rows read from the file.
+The modified list will automatically be written back to the output file.
+ */
+
 public class Action {
 
     public void reverse(List<String> rows) {
